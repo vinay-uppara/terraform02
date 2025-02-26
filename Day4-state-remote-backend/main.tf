3d3f3912c3cca036d9d1e610404c1f5610c0a244
uppara <vinaykumaruppara36@gmail.com>
2025-02-26 12:10:35 +0530

    modified-day4

--- Day4-state-remote-backend/main.tf
+++ Day4-state-remote-backend/main.tf
@@ -1,17 +1,20 @@
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo-db"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
+ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+   name = "terraform-state-lock-dynamo-db"
+   hash_key = "LockID"
+   read_capacity = 20
+   write_capacity = 20
  
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+   attribute {
+     name = "LockID"
+     type = "S"
+   }
+ }
+
+
+resource "aws_s3_bucket" "dev" {
+  bucket = "ashfaqvin"
+  
 }
-
-
-
 
 
 
